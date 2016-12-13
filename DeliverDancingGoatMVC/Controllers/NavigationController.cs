35935d8f2e347bb99bb47b77d3f3c4bef352b0ee
Emmanuel Tissera <emmanuel.tissera@gmail.com>
2016-12-14 10:30:59 +1100

    Added in support to render top navigation from content (not enabled) Upgraded to EmmTi.KenticoCloudConsumer.EnhancedDeliver version 1.0.5

---
+++ DeliverDancingGoatMVC/Controllers/NavigationController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using DeliverDancingGoatMVC.Models;
+using EmmTi.KenticoCloudConsumer.EnhancedDeliver.Factories;
+using System.Web.Mvc;
+using KenticoCloud.Deliver;
+
+namespace DeliverDancingGoatMVC.Controllers
+{
+    public class NavigationController : AsyncController
+    {
+        [ChildActionOnly]
+        public ActionResult TopNavigation()
+        {
+            var filters = new List<IFilter> {
+                new EqualsFilter("system.type", "navigation")
+            };
+
+            //// Filter removed since the HomeViewModel is anyhow cached and reused.
+            var model = DeliverClientFactory<NavigationViewModel>.GetItems(filters);
+            return View(model);
+        }
+    }
+}
